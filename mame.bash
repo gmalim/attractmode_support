12ab9c1a92cf1b44423b7e968012cf7efa710d9f
gmalim <gmalim@users.noreply.github.com>
2018-01-26 10:44:22 -0800

    Update mame.bash

--- mame.bash
+++ mame.bash
@@ -8,11 +8,11 @@
 # 1) Follow instructions in the code of the program above.
 # 2) Change the following two directories according to your setup:
 
-MAMEDIR="/Users/uci/Games/SDLMAME v0.193 64-bit/"                 # Directory containing your mame executable 
-AMSUPPORTDIR="/Users/uci/Programming/Python/attractmode_support/" # Your 'attractmode_support' directory 
+MAMEDIR="${HOME}/SDLMAME v0.193 64-bit/"    # Directory containing your mame executable 
+AMSUPPORTDIR="${HOME}/attractmode_support/" # Your 'attractmode_support' directory 
 
 # 2) Open a terminal and cd to the directory that contains this startup script.
-# 3) Type: source mame.csh {game}
+# 3) Type: source mame.bash {game}
 #
 # OR, instead of steps 2 and 3, create an alias for this startup script in your main .bash startup script, i.e. add the line:
 # alias mame = 'bash ${AMSUPPORTDIR}/mame.bash'
